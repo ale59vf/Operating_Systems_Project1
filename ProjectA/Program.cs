7fc90b36455e22aeb1bffbd68dd7d444572b9697
ale59vf <alessandra.valter.f@gmail.com>
2025-02-25 13:52:36 -0500

    Add project files.

---
+++ ProjectA/Program.cs
@@ -0,0 +1,39 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // print list
+        Console.WriteLine("C# Threading Project - Select Phase:"
+                        + "\n1. Basic Thread Operations"
+                        + "\n2. Resource Thread Operations"
+                        + "\n3. Deadlock Creation"
+                        + "\n4. Dealock Resolution"
+                        + "\nEnter your choice: ");
+        int choice = int.Parse(Console.ReadLine());
+
+        // switch statement for different list options
+        switch (choice) 
+        {
+            case 1:
+                // phase 1 option - Basic Threading
+                //-Phases.Phase1
+                break;
+            case 2:
+                // phase 2 option - Resource Protection
+                //-Phases.Phase3
+                break;
+            case 3:
+                // phase 3 option - Deadlock Creation
+                //-Phases.Phase3
+                break;
+            case 4:
+                // phase 4 option - Deadlock Resolution
+                //-Phases.Phase4
+                break;
+            default:
+                // default option - invalid choice
+                Console.WriteLine("Invalid choice");
+                break;
+        }
+    }
+}
---
+++ ProjectA/bankAccount.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectA
+{
+    internal class bankAccount
+    {
+        // variables
+        private float balance;
+        private float initialBalance;
+        private float depositAmount;
+        private float withdrawAmount;
+
+        // bank account initial balance
+        public bankAccount(float initialBalance)
+        {
+            balance = initialBalance;
+        }
+
+        // deposit function 
+        public void Deposit(float depositAmount){
+            if (balance >= depositAmount){
+                balance -= depositAmount;
+                Console.WriteLine($"Withdrawn {depositAmount}, Remaining Balance: {balance}");
+            }
+            else{Console.WriteLine("Insufficient funds.");}
+        }
+
+        // withdraw function
+        public void Withdraw(float withdrawAmount)
+        {
+            if (balance >= withdrawAmount){
+                balance -= withdrawAmount;
+                Console.WriteLine($"Withdrawn {withdrawAmount}, Remaining Balance: {balance}");
+            }
+            else{Console.WriteLine("Insufficient funds.");}
+        }
+
+        // view balance
+        public float getBalance() { 
+            return balance;
+        }
+    }
+}
