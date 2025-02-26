fdaf493790313441957e5cc479fbe2039ee8f218
ale59vf <alessandra.valter.f@gmail.com>
2025-02-26 16:48:54 -0500

    commit

--- ProjectA/Program.cs
+++ ProjectA/Program.cs
@@ -17,18 +17,25 @@
             case 1:
                 // phase 1 option - Basic Threading
                 //-Phases.Phase1
+                Phases.Phase1_BasicThreads.Run();
                 break;
             case 2:
                 // phase 2 option - Resource Protection
                 //-Phases.Phase3
+                Phases.Phase2_ResourceProtection.Run();
+
                 break;
             case 3:
                 // phase 3 option - Deadlock Creation
                 //-Phases.Phase3
+                Phases.Phase3_DeadlockCreation.Run();
+
                 break;
             case 4:
                 // phase 4 option - Deadlock Resolution
                 //-Phases.Phase4
+                Phases.Phase4_DeadlockResolution.Run();
+        
                 break;
             default:
                 // default option - invalid choice
