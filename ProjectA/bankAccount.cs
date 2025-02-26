d47057c795aa1ebcaa514455d9e20db8b9544249
ale59vf <157517765+ale59vf@users.noreply.github.com>
2025-02-26 10:39:53 -0500

    Revert "Phase 2"

--- ProjectA/bankAccount.cs
+++ ProjectA/bankAccount.cs
@@ -7,70 +7,39 @@ using System.Threading.Tasks;
 
 namespace ProjectA
 {
-     internal class bankAccount
- {
-     // variables
-     private float balance;
-     private float initialBalance;
-     private float totalBalance;
-     private float depositAmount;
-     private float withdrawAmount;
-     // create mutex
-     private Mutex mutex = new Mutex();
+    internal class bankAccount
+    {
+        // variables
+        private float balance;
+        private float initialBalance;
+        private float depositAmount;
+        private float withdrawAmount;
 
-     // bank account initial balance
-     public bankAccount(float initialBalance)
-     {
-         balance = initialBalance;
-     }
+        // bank account initial balance
+        public bankAccount(float initialBalance)
+        {
+            balance = initialBalance;
+        }
 
-     // deposit function 
-     public void Deposit(float depositAmount) { 
-         // lock the mutex
-         mutex.WaitOne();
-         try
-         {
-             Console.WriteLine($"{Thread.CurrentThread.Name} is depositing {depositAmount:C}");
-             totalBalance = balance + depositAmount;
-             Thread.Sleep(1000); // processing time
-             balance = totalBalance;
-             Console.WriteLine($"{Thread.CurrentThread.Name} new balance: {balance:C}");
-         }
-         finally
-         {
-             mutex.ReleaseMutex(); // Release the lock
-         }
-     }
+        // deposit function 
+        public void Deposit(float depositAmount) { 
+            balance += depositAmount;
+            Console.WriteLine($"Withdrawn {depositAmount}, Remaining Balance: {balance}");
+        }
 
-     // withdraw function
-     public void Withdraw(float withdrawAmount)
-     {
-         // lock the mutex
-         mutex.WaitOne();
-         try
-         {
-             if (balance >= withdrawAmount)
-             {
-                 Console.WriteLine($"{Thread.CurrentThread.Name} is withdrawing {withdrawAmount:C}");
-                 totalBalance = balance - withdrawAmount;
-                 Thread.Sleep(1000); // processing time
-                 balance = totalBalance;
-                 Console.WriteLine($"{Thread.CurrentThread.Name} new balance: {balance:C}");
-             }
-             else
-             {
-                 Console.WriteLine($"{Thread.CurrentThread.Name} attempted to withdraw {withdrawAmount:C}, but insufficient funds.");
-             }
-         }
-         finally
-         {
-             mutex.ReleaseMutex(); // Release the lock
-         }
-     }
+        // withdraw function
+        public void Withdraw(float withdrawAmount)
+        {
+            if (balance >= withdrawAmount){
+                balance -= withdrawAmount;
+                Console.WriteLine($"Withdrawn {withdrawAmount}, Remaining Balance: {balance}");
+            }
+            else{Console.WriteLine("Insufficient funds.");}
+        }
 
-     // view balance
-     public float getBalance() { 
-         return balance;
-     }
- }
+        // view balance
+        public float getBalance() { 
+            return balance;
+        }
+    }
 }
