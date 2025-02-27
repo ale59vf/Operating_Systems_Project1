728c816b288e11036dcac12133b55a6fe0936568
ale59vf <alessandra.valter.f@gmail.com>
2025-02-27 13:58:38 -0500

    organization

--- ProjectA/Program.cs
+++ ProjectA/Program.cs
@@ -8,7 +8,8 @@
                         + "\n2. Resource Thread Operations"
                         + "\n3. Deadlock Creation"
                         + "\n4. Dealock Resolution"
-                        + "\nEnter your choice: ");
+                        + "\n5.Quit" 
+                        + "Enter your choice: ");
         int choice = int.Parse(Console.ReadLine());
 
         // switch statement for different list options
@@ -16,26 +17,24 @@
         {
             case 1:
                 // phase 1 option - Basic Threading
-                //-Phases.Phase1
                 Phases.Phase1_BasicThreads.Run();
                 break;
             case 2:
                 // phase 2 option - Resource Protection
-                //-Phases.Phase3
                 Phases.Phase2_ResourceProtection.Run();
 
                 break;
             case 3:
                 // phase 3 option - Deadlock Creation
-                //-Phases.Phase3
                 Phases.Phase3_DeadlockCreation.Run();
 
                 break;
             case 4:
                 // phase 4 option - Deadlock Resolution
-                //-Phases.Phase4
                 Phases.Phase4_DeadlockResolution.Run();
-        
+                break;
+            case 5:
+                Console.WriteLine("Exitting program.....");
                 break;
             default:
                 // default option - invalid choice
